0d6af287bec409bbb2cd166e6a1efdd86ec983c2
Purinutt Amartayavis <ice.ice5335@gmail.com>
2025-12-09 02:45:27 +0700

    Rm: console#log message when connect or disconnect a database

--- packages/databases/src/index.ts
+++ packages/databases/src/index.ts
@@ -4,13 +4,6 @@ if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL env is undefined.");
 }
 
-const connection = new SQL({
-	onconnect: () => {
-		console.log("Connected to PostgreSQL");
-	},
-	onclose: () => {
-		console.log("PostgreSQL connection closed");
-	},
-})
+const connection = new SQL()
 
 export { connection }
