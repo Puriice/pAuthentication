3b5846c280ed8a897947de0554ef1d0961f742e2
Purinutt Amartayavis <ice.ice5335@gmail.com>
2026-01-09 07:46:29 +0700

    Add: bigint config to SQL

--- packages/databases/src/index.ts
+++ packages/databases/src/index.ts
@@ -4,6 +4,8 @@ if (!process.env.DATABASE_URL && process.env.NODE_ENV != 'TEST') {
 	throw new Error("DATABASE_URL env is undefined.");
 }
 
-const pg = new SQL()
+const pg = new SQL({
+	bigint: true,
+})
 
 export { pg }
