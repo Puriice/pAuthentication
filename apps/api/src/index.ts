dcc2cf41862ad7d7d5914e72766cfdd1f12dfb04
Purinutt Amartayavis <ice.ice5335@gmail.com>
2026-01-13 04:37:39 +0700

    Add: API versioning

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,19 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const prefix = (prefix: string, elysia: Elysia<any, any, any>) => new Elysia()
+	.group(prefix, app => app.use(elysia))
+
+const version = (version: number) => prefix(`/v${version}`,
+	new Elysia()
+		.decorate('version', version)
+		.get('/', ({ version }) => version)
+)
+
+const app = new Elysia()
+	.use(prefix('/api', version(1)))
+	.get("/", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
